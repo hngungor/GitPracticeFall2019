14894b6f9afc770e2e94841d50ce22b33791af67
hgungor <hngungor@hotmail.com>
2020-03-01 17:53:43 -0500

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -2,10 +2,12 @@ package day1;
 
 public class LoopPractice {
 
+
     public static void main(String[] args) {
 
         for (int i=0; i<10; i+=2){
             System.out.println("counter :: "+i);
+            System.out.println("))))");
         }
 
     }
