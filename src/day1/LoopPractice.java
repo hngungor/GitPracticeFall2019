fc8f3aa249fd60dfc97d4cbb15d4511e72bd91e1
hgungor <hngungor@hotmail.com>
2020-03-25 21:47:36 -0400

    Added Colors class

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -5,9 +5,11 @@ public class LoopPractice {
 
     public static void main(String[] args) {
 
+        //some random code
         for (int i=0; i<10; i+=2){
             System.out.println("counter :: "+i);
             System.out.println("))))");
+            System.out.println("git is fun:(");
         }
 
     }
